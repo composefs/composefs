d0dc993a45ed70e26694cbe47978d009dd905374
Colin Walters <walters@verbum.org>
2025-04-08 20:14:44 -0400

    tests/should-fail: Verify exit code 1
    Some of these runs were exiting with SIGSEGV but because we
    were just checking that the command failed, we masked that error...
    Fix the test code here to verify that the process exited with code 1.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- tests/test-should-fail.sh
+++ tests/test-should-fail.sh
@@ -7,8 +7,14 @@ shift
 tmpd=$(mktemp -d -t lcfs-test.XXXXXX)
 trap 'rm -rf -- "$tmpd"' EXIT
 for f in $@; do
-    if $bindir/mkcomposefs --from-file $f $tmpd/out.cfs &>/dev/null; then
+    rc=0
+    $bindir/mkcomposefs --from-file $f $tmpd/out.cfs >/dev/null 2>$tmpd/err.txt || rc=$?
+    if test $rc == 0; then
         fatal "Test case $f should have failed"
+    fi
+    if test $rc != 1; then
+        cat $tmpd/err.txt
+        fatal "Test case $f exited with code $rc, not 1"
     fi
     echo "ok $f"
 done
